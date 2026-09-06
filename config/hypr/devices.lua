36459bb8c5a8d0125283b26f45fedf98f170460c
Joel Kuehne <joelckuehne@gmail.com>
2026-09-05 19:49:27 -0500

    Updated hyprland devices

--- config/hypr/devices.lua
+++ config/hypr/devices.lua
@@ -1,4 +1,5 @@
 
+-- Sensible device defaults
 hl.config({
     input = {
         kb_layout  = "us",
@@ -15,5 +16,22 @@ hl.config({
     },
 })
 
+-- Configure wacom tablet
+hl.device({ name = "wacom-one-by-wacom-s-pen", output = "DP-1" })
+
+-- Configure monitors
 hl.monitor({ output = "DP-1", mode = "highrr", position = "1920x0", scale = 1 })
 hl.monitor({ output = "DP-2", mode = "highrr", position = "0x0", scale = 1 })
+
+-- Pin workspaces to monitors
+hl.workspace_rule({ workspace = "1", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "2", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "3", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "4", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "5", monitor = "DP-1" })
+hl.workspace_rule({ workspace = "6", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "7", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "8", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "9", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "0", monitor = "DP-2" })
+
