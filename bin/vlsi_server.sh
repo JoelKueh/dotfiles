04ccd33abd753151d9df6b969c978d5822428694
Joel Kuehne <joelckuehne@gmail.com>
2026-09-19 13:00:55 -0500

    Fixed vlsi_server.sh

--- bin/vlsi_server.sh
+++ bin/vlsi_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 HOST_NUMBER="${1:-18}"
+PORT=$(printf "330%02d" "$HOST_NUMBER")
+HOST=$(printf "ece-kh2120-%02d.ece.umn.edu:22" "$HOST_NUMBER")
 
-fuser -k 33022/tcp 2>/dev/null
-nohup socat "TCP-LISTEN:330$HOST_NUMBER",bind=127.0.0.1,reuseaddr,fork \
-	"SYSTEM:'ssh -W ece-kh2120-${HOST_NUMBER}.ece.umn.edu:22 vlsi-jump'" &>/dev/null &
+fuser -k $PORT/tcp 2>/dev/null
+nohup socat "TCP-LISTEN:$PORT",bind=127.0.0.1,reuseaddr,fork \
+	"SYSTEM:'ssh -W $HOST vlsi-jump'" &>/dev/null &
